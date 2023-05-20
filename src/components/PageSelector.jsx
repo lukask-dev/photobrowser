682fde16812601fec78b9c2b99159d1dda60ed43
Lukas <lukas.kallenbach@gmail.com>
2023-05-20 19:27:03 +0300

    input field more mobile friendly

--- src/components/PageSelector.jsx
+++ src/components/PageSelector.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function PageSelector({ page, setNewPageNumber, lastPage }) {
 
   const leftButtonDisabled = page <= 1;
   const rightButtonDisabled = page >= lastPage;
+  const [inputFieldValue, setInputFieldValue] = useState(page.toString());  
 
   function handleInputFieldChange(event) {
-    setNewPageNumber(parseInt(event.target.value));
+    const newValue = event.target.value;
+    setInputFieldValue(newValue);
+    const parsed = parseInt(newValue);
+    if (parsed && parsed > 0)
+      setNewPageNumber(parseInt(newValue));
+  }
+
+  function handeArrowClick(newPageNumber) {
+    setInputFieldValue(newPageNumber);
+    setNewPageNumber(newPageNumber);
   }
 
   function handleFocus(event) {
     event.target.select();
   }
 
+  function handleBlur(event) {
+    if (inputFieldValue !== page)
+      setInputFieldValue(page);
+  }
+
   return (
     <div className="page-selector-container">
       <div className="page-selector">
         <button
-          onClick={() => setNewPageNumber(Number(page) - Number(1))}
+          onClick={() => handeArrowClick(Number(page) - Number(1))}
           className={leftButtonDisabled ? "button-disabled arrow-button-left" : "button arrow-button-left"}
           title="Previous page"
           disabled={leftButtonDisabled}
@@ -27,15 +42,16 @@ function PageSelector({ page, setNewPageNumber, lastPage }) {
         <input
           className="page-input"
           type="text"
-          value={page}
+          value={inputFieldValue}
           onChange={handleInputFieldChange}
           onFocus={handleFocus}
+          onBlur={handleBlur}
           inputMode="numeric"
           pattern="[0-9]*"
           title="Enter page number"
         />
         <button
-          onClick={() => setNewPageNumber(Number(page) + Number(1))}
+          onClick={() => handeArrowClick(Number(page) + Number(1))}
           className={rightButtonDisabled ? "button-disabled arrow-button-right" : "button arrow-button-right"}
           title="Next page"
           disabled={rightButtonDisabled}
